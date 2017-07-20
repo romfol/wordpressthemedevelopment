3b7ab54181db1e72c50d9a84b2bd79c77b62b7f2
Roman <romfoll@users.noreply.github.com>
2017-07-20 17:45:43 +0300

    Child Page Menu (parent & subpages)

--- wp-content/themes/learningWordPress/functions.php
+++ wp-content/themes/learningWordPress/functions.php
@@ -13,3 +13,26 @@ register_nav_menus(array(
 	'primary' => __('Primary Menu'),
 	'footer' => __('Footer Menu')
 ));
+
+//Get the ancestor
+function get_top_ancestor_id() {
+
+	global $post;
+
+	if ($post->post_parent) {
+		$ancestors = array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];	
+	}
+
+	return $post->ID;
+}
+
+// Does page has children? 
+function has_children() {
+	
+	global $post;
+
+	$pages = get_pages('child_of=' . $post->ID);
+	return count($pages);
+
+}
